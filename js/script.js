3a750570818b5f71e3391f6e70b9bad3c22a2c72
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-02-22 15:40:02 +0100

    Inizializzazione

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+1 chiedere quanti km deve fare il passeggero
+2 chiedere l'età del passeggero
+3 definire il prezzo del biglietto di 0.21€/km tenendo conto di:
+  - sconto 20% per minorenni (- di 18 anni)
+  - sconto 40% per gli over 65 anni
+  4 mostrare prezzo con una frase umana
+*/
