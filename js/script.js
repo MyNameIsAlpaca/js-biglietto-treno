fde84345193aa39427593b3037fb47a1c9d79ef5
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-02-22 16:57:13 +0100

    Feat: add js command

--- js/script.js
+++ js/script.js
@@ -4,5 +4,27 @@
 3 definire il prezzo del biglietto di 0.21€/km tenendo conto di:
   - sconto 20% per minorenni (- di 18 anni)
   - sconto 40% per gli over 65 anni
-  4 mostrare prezzo con una frase umana
+4 mostrare prezzo con una frase umana
 */
+
+let passengerAge = parseInt(prompt("Quanti anni hai?"))
+
+let distance = parseInt(prompt("Quanti km devi percorrere?"))
+
+let ticketPrice = (distance * 0.21)
+
+if (passengerAge < 18) {
+    ticketPrice =  Math.round((((distance * 0.21) * 80) / 100) * 100) / 100;  
+} 
+else if (passengerAge > 65) {
+    ticketPrice =  Math.round((((distance * 0.21) * 60) / 100) * 100) / 100;  
+}
+
+document.writeln( "Il costo del tuo viaggio è di " + ticketPrice + "€.")
+
+if (passengerAge < 18) {
+    document.writeln("Visto che la tua età è di " + passengerAge + " anni hai ricevuto uno sconto del 20%!")
+} 
+else if (passengerAge > 65) {
+    document.writeln("Visto che la tua età è di " + passengerAge + " anni hai ricevuto uno sconto del 40%!")
+}
